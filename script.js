284422c8a998878c82b3b91ebc666139b8cab0b6
Ali Hashish <alimohamedhashish@gmail.com>
2023-04-13 09:49:56 +0200

    commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".main-header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    //TO close navigation when link is clicked
+
+    if (link.classList.contains("nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
